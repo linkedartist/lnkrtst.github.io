77c3016e6c7d8cf9cb846084e4518dd2ce00026f
linkedartist <113187562+linkedartist@users.noreply.github.com>
2023-04-17 08:34:19 +0000

    Add files via upload

---
+++ gitprofile.config.js
@@ -0,0 +1,151 @@
+// gitprofile.config.js
+
+const config = {
+  github: {
+    username: 'linkedartist', // Your GitHub org/user name. (Required)
+    sortBy: 'stars', // stars | updated
+    limit: 100, // How many projects to display.
+    exclude: {
+      forks: true, // Forked projects will not be displayed if set to true.
+      projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
+    },
+  },
+  social: {
+    linkedin: '',
+    twitter: '',
+    mastodon: '',
+    facebook: '',
+    instagram: '',
+    dribbble: '',
+    behance: '',
+    medium: '',
+    dev: 'linkedartist',
+    stackoverflow: '', // format: userid/username
+    skype: '',
+    telegram: '',
+    website: '',
+    phone: '',
+    email: 'lnkrtst@gmail.com',
+  },
+  resume: {
+    fileUrl:
+      '', // Empty fileUrl will hide the `Download Resume` button.
+  },
+  skills: [
+    'PHP',
+    'JavaScript',
+    'Node.js',
+    'MySQL',
+    'MTML',
+    'Git',
+    'Html',
+    'Css',
+    'JS',
+    'Bash'
+  ],
+  experiences: [
+    {
+      company: '  Pylex',
+      position: 'Moderator/Support Agent',
+      from: 'January',
+      to: 'Present',
+      companyLink: 'https://pylexnodes.net/',
+    }
+  ],
+  /* certifications: [
+    {
+      name: '',
+      body: '',
+      year: '',
+      link: ''
+    },
+  ], */
+  education: [
+    {
+      institution: '',
+      degree: '',
+      from: '',
+      to: '',
+    },
+    {
+      institution: '',
+      degree: '',
+      from: '',
+      to: '',
+    },
+  ],
+
+  // To hide the `My Projects` section, keep it empty.
+  googleAnalytics: {
+    id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
+  },
+  // Track visitor interaction and behavior. https://www.hotjar.com
+  hotjar: {
+    id: '',
+    snippetVersion: 6,
+  },
+  themeConfig: {
+    defaultTheme: 'dark',
+
+    // Hides the switch in the navbar
+    // Useful if you want to support a single color mode
+    disableSwitch: false,
+
+    // Should use the prefers-color-scheme media-query,
+    // using user system preferences, instead of the hardcoded defaultTheme
+    respectPrefersColorScheme: false,
+
+    // Hide the ring in Profile picture
+    hideAvatarRing: false,
+
+    // Available themes. To remove any theme, exclude from here.
+    themes: [
+      'light',
+      'dark',
+      'cupcake',
+      'bumblebee',
+      'emerald',
+      'corporate',
+      'synthwave',
+      'retro',
+      'cyberpunk',
+      'valentine',
+      'halloween',
+      'garden',
+      'forest',
+      'aqua',
+      'lofi',
+      'pastel',
+      'fantasy',
+      'wireframe',
+      'black',
+      'luxury',
+      'dracula',
+      'cmyk',
+      'autumn',
+      'business',
+      'acid',
+      'lemonade',
+      'night',
+      'coffee',
+      'winter',
+      'procyon',
+    ],
+
+    // Custom theme
+    customTheme: {
+      primary: '#fc055b',
+      secondary: '#219aaf',
+      accent: '#e8d03a',
+      neutral: '#2A2730',
+      'base-100': '#E3E3ED',
+      '--rounded-box': '3rem',
+      '--rounded-btn': '3rem',
+    },
+  },
+
+  // Optional Footer. Supports plain text or HTML.
+  footer: `Coding Is nice `,
+};
+
+export default config;
